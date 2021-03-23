6611f3a05a824206be8872de96c396af7ebfe7e5
Ihor Kroosh <ihor.kroosh@gmail.com>
2021-03-23 17:58:00 +0200

    update to be compatible with the new flask dance

--- dash_okta_auth/okta_oauth.py
+++ dash_okta_auth/okta_oauth.py
@@ -1,3 +1,5 @@
+from functools import partial
+
 from oauthlib.oauth2.rfc6749.errors import InvalidGrantError, TokenExpiredError, OAuth2Error
 from flask import (
     redirect,
@@ -6,26 +8,40 @@ from flask import (
     abort,
     session,
 )
-from flask_dance.contrib.okta import (
-  make_okta_blueprint,
-  okta
-)
+
+from flask.globals import LocalProxy, _lookup_app_object
+try:
+    from flask import _app_ctx_stack as stack
+except ImportError:
+    from flask import _request_ctx_stack as stack
+from flask_dance.consumer import OAuth2ConsumerBlueprint
 
 from .auth import Auth
 
+
+okta = LocalProxy(partial(_lookup_app_object, "okta_oauth"))
+
+
 class OktaOAuth(Auth):
-    def __init__(self, app, base_url, additional_scopes=None):
+    def __init__(self, app, base_url):
         super(OktaOAuth, self).__init__(app)
-        okta_bp = make_okta_blueprint(
+        scope = ["openid", "email", "profile"]
+        okta_bp = OAuth2ConsumerBlueprint(
+            "okta",
+            __name__,
+            scope=scope,
             base_url=base_url,
-            authorization_url=base_url + '/oauth2/v1/authorize',
-            token_url=base_url + '/oauth2/v1/token', 
-            scope=[
-                "openid",
-                "email",
-                "profile",
-            ] + (additional_scopes if additional_scopes else [])
+            token_url=base_url + '/oauth2/v1/token',
+            authorization_url=base_url + '/oauth2/v1/authorize'
         )
+        okta_bp.from_config["client_id"] = "OKTA_OAUTH_CLIENT_ID"
+        okta_bp.from_config["client_secret"] = "OKTA_OAUTH_CLIENT_SECRET"
+
+        @okta_bp.before_app_request
+        def set_applocal_session():
+            ctx = stack.top
+            ctx.okta_oauth = okta_bp.session
+
         app.server.register_blueprint(okta_bp, url_prefix="/login")
 
     def is_authorized(self):
